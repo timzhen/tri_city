a94c996bccb83d905a248b96b2419a6acdb975e6
Tim Zhen <timzhen@ucsb.edu>
2025-12-28 16:30:29 -0800

    added church logo to webpage header and favicon and changed color of volunteer button to black

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,11 @@ const Header = ({ currentPage, setCurrentPage }) => {
     <header className="header">
       <div className="header-container">
         <div className="logo">
-          <div className="logo-main">TRI-CITY</div>
-          <div className="logo-sub">FREE BREAKFAST PROGRAM</div>
+          <img src="https://web.archive.org/web/20210802122259im_/https://sites.google.com/site/tricitybreakfast/_/rsrc/1362644090280/config/customLogo.gif?revision=2" alt="Tri-City Free Breakfast Program Logo" className="logo-image" />
+          <div className="logo-text">
+            <div className="logo-main">TRI-CITY</div>
+            <div className="logo-sub">FREE BREAKFAST PROGRAM</div>
+          </div>
         </div>
         <nav className="navigation">
           <button 
